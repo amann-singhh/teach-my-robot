038e1f8c561d60b392b856a6851a64bc2cf3fe72
amannsinghh-cyber <amansinghssor@gmail.com>
2026-01-07 17:21:05 +0530

    username login

--- src/awsConfig.ts
+++ src/awsConfig.ts
@@ -3,10 +3,10 @@ import { Amplify } from "aws-amplify";
 Amplify.configure({
   Auth: {
     Cognito: {
-      userPoolId: "ap-south-1_JcWvJMRVQ",
-      userPoolClientId: "537uc6ucuks1iqhp9fjrhoaiht",
+      userPoolId: "ap-south-1_sR1cnllwN",
+      userPoolClientId: "4ojd8hh7q13fa67gs3iniu1jkj",
       loginWith: {
-        email: true,
+        username: true,
       },
     },
   },
